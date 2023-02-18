6f0dd378d9641735a54025cdad6051e10164419b
tinuola <24995224+tinuola@users.noreply.github.com>
2023-02-17 18:20:07 -0800

    Add hover state to btn

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+
+// create object for all fields
+let firstNameInput = document.getElementById('firstname-input')
+let lastNameInput = document.getElementById('lastname-input')
+let emailInput = document.getElementById('email-input')
+let passwordInput = document.getElementById('password-input')
+let submitBtn = document.getElementById('submit-btn')
+
+// check if fields are empty
+submitBtn.addEventListener('click', function(){
+  console.log('Hello')
+})
+
+// if empty, add error style, icon and error message
